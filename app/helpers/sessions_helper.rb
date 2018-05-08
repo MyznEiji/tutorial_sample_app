eeb810d973a9130db17f8bb340d91bc961513d21
MyznEiji <em711th@icloud.com>
2018-05-08 20:53:34 +0900

    before : all user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,13 @@ module SessionsHelper
       cookies.permanent[:remember_token] = user.remember_token
     end
 
+
+    # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+    def current_user?(user)
+      user == current_user
+    end
+
+
     # 記憶トークンcookieに対応するユーザーを返す
     def current_user
       if (user_id = session[:user_id])
@@ -35,7 +42,7 @@ module SessionsHelper
       session.delete(:user_id)
       @current_user = nil
     end
-        # 永続的セッションを破棄する
+    # 永続的セッションを破棄する
     def forget(user)
       user.forget
       cookies.delete(:user_id)
@@ -48,5 +55,17 @@ module SessionsHelper
       session.delete(:user_id)
       @current_user = nil
     end
+
+    # 記憶したURL (もしくはデフォルト値) にリダイレクト
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+
+    # アクセスしようとしたURLを覚えておく
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
+
 
   end
